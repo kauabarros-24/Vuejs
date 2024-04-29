402623ff4b55af4ab1dd415d8dfe5133d172c097
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-04-29 14:38:37 -0300

    API de composição

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
-<script>
-export default {
-  data() {
-    return {
-      contador: 0
-    }
-  },
-  methods: {
-    incrementarContador() {
-      this.contador++
-    },
-    decrementarContador() {
-      if (this.contador > 0 ) {
-        this.contador--;
-      }
-      else {
-        alert("Contador menor que zero não pode ser decrementado")
-      }
-    }
-  },
-  mounted() {
-    console.log(`O valor inicial do contador é ${this.contador}.`)
-  }
+<script setup>
+import { ref, onMounted } from 'vue';
+const contador = ref(0)
+
+function incrementarContador() {contador.value++}
+function decrementarContador() {if (contador.value > 0){contador.value-- }
+
 }
 </script>
 
