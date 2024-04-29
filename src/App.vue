9dc0d8fbd84fff029fb8d8405e322f4f55ea71f3
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-04-29 14:55:11 -0300

    Resolução do exercício de contadores usando api de opções

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
-<script setup>
-import { ref, onMounted } from 'vue';
-const contador = ref(0)
-
-function incrementarContador() {contador.value++}
-function decrementarContador() {if (contador.value > 0){contador.value-- }
-
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    incrementarContador() {
+      this.contador++
+    },
+    decrementarContador() {
+      if (this.contador > 0) {
+        this.contador--
+      }
+    },
+    zerar() {
+      this.contador = 0
+    }
+  },
 }
+
 </script>
 
 <template>
   <button @click="incrementarContador">Incrementar</button>
   <button @click="decrementarContador">Decrementar</button>
+  <button @click="zerar">Zerara</button>
   <p>O valor do contador é: {{ contador }}</p>
 </template>
 
